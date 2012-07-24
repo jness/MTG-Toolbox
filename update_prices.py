c2c02eed5a44352d558890ce9eeab04f3f72d443
Jeffrey Ness <jeffrey.ness@rackspace.com>
2012-07-24 14:02:53 -0500

    a script to update prices

---
+++ update_prices.py
@@ -0,0 +1,25 @@
+from django.core.management import setup_environ
+import settings
+setup_environ(settings)
+
+from WebMTG.models import MTGPrice, MTGCard
+from magiccardsinfo.Price import Price
+
+# All cards in our collection
+cards = MTGCard.objects.all()
+
+for card in cards:
+
+    print 'Looking up price for %s with tcgplayer_id %s' % (card.card_name, 
+                                                            card.tcgplayer_id)
+
+    p = Price(id=card.tcgplayer_id)
+    prices = p.getPrices()
+
+    print 'Found the following prices %s' % prices
+
+    # Update with the price
+    MTGPrice.objects.create(card=card, low=prices['low'],
+                            avg=prices['avg'], high=prices['high'])
+
+    print 'Successfully updated Price database'
