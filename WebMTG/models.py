ba05e47b580214da08a0cbe9aa309ff30918a2e2
Jeffrey Ness <jeffrey.ness@rackspace.com>
2012-07-25 17:50:29 -0500

    remove model cache

--- WebMTG/models.py
+++ WebMTG/models.py
@@ -1,19 +1,16 @@
 from django.db import models
-import caching.base
 
-class MTGSet(caching.base.CachingMixin, models.Model):
+class MTGSet(models.Model):
     label = models.CharField(max_length=75, unique=True)
     display_name = models.CharField(max_length=75)
     magiccards_info = models.CharField(max_length=10, null=True)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
     
-    objects = caching.base.CachingManager()
-    
     def __unicode__(self):
         return self.display_name
     
-class MTGCard(caching.base.CachingMixin, models.Model):
+class MTGCard(models.Model):
     magiccard_id = models.CharField(max_length=10)
     gatherer_id = models.IntegerField()
     tcgplayer_id = models.IntegerField()
@@ -25,20 +22,16 @@ class MTGCard(caching.base.CachingMixin, models.Model):
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
     
-    objects = caching.base.CachingManager()
-    
     def __unicode__(self):
         return self.card_name
     
-class MTGPrice(caching.base.CachingMixin, models.Model):
+class MTGPrice(models.Model):
     card = models.ForeignKey(MTGCard)
     low = models.CharField(max_length=20)
     avg = models.CharField(max_length=20)
     high = models.CharField(max_length=20)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
-    
-    objects = caching.base.CachingManager()
     
     def __unicode__(self):
         return self.card.card_name
