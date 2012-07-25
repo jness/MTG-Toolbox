cc00a01785e47d84f805af23be101c7c5fa7268e
Jeffrey Ness <jeffrey.ness@rackspace.com>
2012-07-25 11:54:11 -0500

    tcgplayer does not use small set names..

--- WebMTG/models.py
+++ WebMTG/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class MTGSet(models.Model):
-    label = models.CharField(max_length=10, unique=True)
+    label = models.CharField(max_length=75, unique=True)
     display_name = models.CharField(max_length=75)
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
