4ca4c3ad6a947a7ee4c40971c282463ac2f96da8
Jeffrey Ness <jeffrey.ness@rackspace.com>
2012-07-26 10:14:12 -0500

    set trends

--- WebMTG/templatetags/tools.py
+++ WebMTG/templatetags/tools.py
@@ -1,5 +1,6 @@
 from django import template
 from django.conf import settings
+from WebMTG.models import MTGPrice
 
 register = template.Library()
 
@@ -7,3 +8,14 @@ register = template.Library()
 def price(number):
     p = "%.2f" % round(number,2)
     return p
+
+@register.filter()
+def upordown(card):
+    latest = MTGPrice.objects.filter(card=card).latest('created')
+    if latest.avg > card.avg:
+        return '<font size=1 color=red>Down</font> <i class=icon-arrow-down></i>'
+    elif latest.avg < card.avg:
+        return '<font size=1 color=#33CC33>Up</font> <i class=icon-arrow-up></i>'
+    else:
+        return '<font size=1 color=grey>No Change</font>'
+        
