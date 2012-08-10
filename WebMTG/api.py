735600f7cb1da1fea7fe2c11ecbaaa4bbc032ddf
Jeffrey Ness <jeffrey.ness@rackspace.com>
2012-08-10 12:37:16 -0500

    order api calls

--- WebMTG/api.py
+++ WebMTG/api.py
@@ -6,13 +6,13 @@ from WebMTG.models import MTGCard, MTGSet
         
 class MTGSetResource(ModelResource):
     class Meta:
-        queryset = MTGSet.objects.all()
+        queryset = MTGSet.objects.all().order_by('display_name')
         resource_name = 'set'
     
 class MTGCardResource(ModelResource):
     set = fields.ForeignKey(MTGSetResource, 'set', full=True)
     class Meta:
-        queryset = MTGCard.objects.all()
+        queryset = MTGCard.objects.all().order_by('card_name')
         resource_name = 'card'
         filtering = {
                 'card_name': ('exact', 'contains', 'icontains'),
