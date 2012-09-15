46d87e4e9e8b2e2c2904b4a437c0c2c8f01314e3
Jeffrey Ness <jness@flip-edesign.com>
2012-09-15 12:17:53 -0500

    move class to bottom

--- WebMTG/models.py
+++ WebMTG/models.py
@@ -1,13 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class UserWatch(models.Model):
-    card = models.ForeignKey(MTGCard)
-    user = models.ForeignKey(User)
-    
-    def __unicode__(self):
-        return self.user
-
 class MTGSet(models.Model):
     label = models.CharField(max_length=75, unique=True)
     display_name = models.CharField(max_length=75)
@@ -17,7 +10,6 @@ class MTGSet(models.Model):
     
     def __unicode__(self):
         return self.display_name
-    
     
 class MTGCard(models.Model):
     magiccard_id = models.CharField(max_length=10)
@@ -67,3 +59,9 @@ class MTGHash(models.Model):
     def __unicode__(self):
         return self.card.card_name
     
+class UserWatch(models.Model):
+    card = models.ForeignKey(MTGCard)
+    user = models.ForeignKey(User)
+    
+    def __unicode__(self):
+        return self.user
